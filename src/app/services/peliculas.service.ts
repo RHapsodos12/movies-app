09e3df44953785fedca34abd84148d3751f8581e
David Santacruz <davidsantacruz09@gmail.com>
2021-09-28 13:16:04 -0500

    + peliculas service

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CarteleraResponse } from '../interfaces/cartelera-respones';
+
+import { Observable, of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
+
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-respones';
+import { MovieResponse } from '../interfaces/movie-response';
+import { Cast, CreditsResponse } from '../interfaces/credits-response';
+import { environment } from '../../environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +15,76 @@ import { CarteleraResponse } from '../interfaces/cartelera-respones';
 
 export class PeliculasService {
 
-  constructor( private http: HttpClient ) {
+  // tslint:disable-next-line: ban-types
+  private url: String = 'https://api.themoviedb.org/3';
+  private page = 1;
+  public cargando = false;
+
+  constructor( private http: HttpClient ) { }
+
+  // tslint:disable-next-line: typedef
+  get params() {
+    return {
+      api_key: environment.apiKey,
+      language: 'es-ES',
+      page: this.page.toString(),
+    };
+  }
+
+  // tslint:disable-next-line: typedef
+  resetCarteleraPage() {
+    this.page = 1;
+  }
+
+  // tslint:disable-next-line: typedef
+  getCartelera(): Observable<Movie[]> {
+
+    if ( this.cargando) {
+      return of( [] );
+    }
+
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${ this.url }/movie/now_playing`, {
+      params: this.params
+    }).pipe(
+      map( (resp) => resp.results),
+      tap( () => {
+        this.page += 1;
+        this.cargando = false;
+      })
+    );
 
   }
 
   // tslint:disable-next-line: typedef
-  getCartelera(): Observable<CarteleraResponse> {
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=6966d54b746f292ab89eb31db8dc6a5f&language=es-ES&page=1');
+  buscarPeliculas( texto: string ): Observable<Movie[]> {
+    const params = { ...this.params, page: '1', query: texto };
+
+    return this.http.get<CarteleraResponse>(`${ this.url }/search/movie`, {
+      params
+    }).pipe(
+      map( resp => resp.results )
+    );
   }
+
+  // tslint:disable-next-line: typedef
+  getPeliculaDetalle( id: string ) {
+    return this.http.get<MovieResponse>(`${ this.url }/movie/${ id }`, {
+      params: this.params
+    }).pipe(
+      catchError( err => of(null))
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  getCast( id: string ): Observable<Cast[]> {
+    return this.http.get<CreditsResponse>(`${ this.url }/movie/${ id }/credits`, {
+      params: this.params
+    }).pipe(
+      map( resp => resp.cast ),
+      catchError( err => of([]))
+    );
+  }
+
 }
