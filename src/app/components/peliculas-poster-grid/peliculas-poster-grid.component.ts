7626898c1e37dde76249114c9bdad870a24b3704
David Santacruz <davidsantacruz09@gmail.com>
2021-09-28 13:12:06 -0500

    + peliculas poster grid

--- src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
+++ src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from 'src/app/interfaces/cartelera-respones';
+
+import { StarRatingComponent } from 'ng-starrating';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-peliculas-poster-grid',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasPosterGridComponent implements OnInit {
 
-  constructor() { }
+  @Input() movies: Movie[];
+
+  constructor( private router: Router ) { }
 
   ngOnInit(): void {
+    // console.log(this.movies);
+  }
+
+  // tslint:disable-next-line: typedef
+  onRate( $event: {oldValue: number, newValue: number, starRating: StarRatingComponent } ) {
+    alert(`Old Value:${$event.oldValue},
+      New Value: ${$event.newValue},
+      Checked Color: ${$event.starRating.checkedcolor},
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+  }
+
+  // tslint:disable-next-line: typedef
+  onMovieClick( movie: Movie ) {
+    // console.log(movie);
+    this.router.navigate(['/pelicula', movie.id]);
   }
 
 }
